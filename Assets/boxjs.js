3e806407a5e17f59aaf40c2041f6efbafdd339e5
cwild@imisshtml.com <cwild@imisshtml.com>
2013-11-10 18:20:44 -0500

    setting up android apk and accelerometer

--- Assets/boxjs.js
+++ Assets/boxjs.js
@@ -1,9 +1,19 @@
 ﻿#pragma strict
 
+var speed:int = 5;
+
 function Start () {
 
 }
 
 function Update () {
+	var dir : Vector3 = Vector3.zero;
+	dir.x = -Input.acceleration.y;
+	dir.z = Input.acceleration.x;
 
+	if (dir.sqrMagnitude > 1)
+		dir.Normalize();
+
+	dir *= Time.deltaTime;
+	transform.Translate (dir * speed);
 }
