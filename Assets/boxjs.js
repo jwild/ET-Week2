b19cc78e2f1e261f3694a23bfa3b6baf50a57adc
cwild@imisshtml.com <cwild@imisshtml.com>
2013-11-10 18:40:00 -0500

    accelerometer work

--- Assets/boxjs.js
+++ Assets/boxjs.js
@@ -1,12 +1,38 @@
 ﻿#pragma strict
 
-var speed:int = 5;
+var speed:int = 10;
+var filter:float = 5;
+var maxX:float=10;
+var maxY:float=10;
+var minX:float=-10;
+var minY:float=-10;
+
+private var accel:Vector3;
 
 function Start () {
-
+	accel = Input.acceleration;
 }
 
 function Update () {
+	Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	
+	accel = Vector3.Lerp(accel, Input.acceleration, filter*Time.deltaTime);
+	
+	var dir=Vector3(-accel.y, accel.x, 0);
+	if(dir.sqrMagnitude > 1){
+		dir.Normalize();
+	}
+		
+	transform.Translate(dir*speed*Time.deltaTime);	
+
+	var pos = transform.position;
+	pos.x = Mathf.Clamp(pos.x, minX, maxX);
+	pos.y = Mathf.Clamp(pos.y, minY, maxY);
+	
+	transform.position = pos;
+
+	
+/*
 	var dir : Vector3 = Vector3.zero;
 	dir.x = -Input.acceleration.y;
 	dir.z = Input.acceleration.x;
@@ -16,4 +42,5 @@ function Update () {
 
 	dir *= Time.deltaTime;
 	transform.Translate (dir * speed);
+	*/
 }
